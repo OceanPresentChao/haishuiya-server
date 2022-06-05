cd01165df4390a446f9839791d442c1384b36592
OceanPresent <oceanpresent@163.com>
2022-06-05 10:49:01 +0800

    :recycle: refactor: bin/www

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const routes = require('./src/routes');
 const bodyParser = require('body-parser')
 
 const app = express();
-const port = 2777;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', cors());
 app.use('/', routes);
 
-app.listen(port, () => { console.log(`服务器已启动','http://localhost:${port}`); })
+module.exports = app
